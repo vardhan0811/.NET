118892d9247e497abd6bee25564f07e8ce0db9e6
Vardhan Rayapureddy <rayapureddyvardhan2004@gmail.com>
2025-12-18 16:50:14 +0530

    Rename Program.cs to Day1.cs and update entry point

--- Day1.cs
+++ Day1.cs
@@ -1,18 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
 using System;
-class Program
+class Day1
 {
     static void Main(string[] args)
     {
-        // prime number code using sieve of eratosthenes
-        Console.Write("Enter a number: ");
-        string? input = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(input))
+        Console.WriteLine("Enter a number:");
+        if (!int.TryParse(Console.ReadLine(), out int n))
         {
             Console.WriteLine("Invalid input.");
             return;
         }
-        int n = int.Parse(input);
         bool[] isPrime = new bool[n + 1];
         for (int i = 2; i <= n; i++)
             isPrime[i] = true;
@@ -24,13 +20,12 @@ class Program
                     isPrime[i] = false;
             }
         }
-        Console.WriteLine($"Prime numbers up to {n}: ");
+        Console.WriteLine($"Prime numbers up to {n}:");
         for (int i = 2; i <= n; i++)
         {
             if (isPrime[i])
                 Console.Write(i + " ");
-        }        
+        }
+        Console.WriteLine();
     }
 }
-
-
